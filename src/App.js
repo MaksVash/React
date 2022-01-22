1b1adffb74a11cd10b407087d00db20dc2df2653
MaksVash <88022630+MaksVash@users.noreply.github.com>
2022-01-22 22:45:03 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
+import Heder from './components/Heder/Heder';
+import Musik from './components/Musik/Musik';
+import Navbar from './components/Navbar/Navbar';
+import News from './components/News/News';
+import Profile from './components/Profile/Profile';
+import Seting from './components/Seting/Seting';
+
+
+
+
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <div className='app-wraper'>
+        <Heder />
+        <Navbar />
+
+        <div className='app-wraper-content'>
+          <Routes>
+            <Route exact path='/dialogs/*' element={<Dialogs />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/seting' element={<Seting />} />
+            <Route path='/musik' element={<Musik />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+
+  )
+}
+export default App;
---
+++ src/components/Musik/Musik.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import s from'./Musik.module.css';
+const Musik = ()=>{
+    return (
+      <div>Musik</div>
+
+    )
+}
+export default Musik;
