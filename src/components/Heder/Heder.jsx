3af900ca82f14aa28709695ac06f9008aa76d4b3
MaksVash <88022630+MaksVash@users.noreply.github.com>
2022-01-30 13:08:07 +0200

    Delete src directory

--- src/components/Heder/Heder.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import s from'./Heder.module.css';
-
-const Heder = (props)=>{
-    return <header className = {s.heder}>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPxP88dkJqjPmshDwNRdAQ91-n3FnW95qznw&usqp=CAU' height='70px'></img>
-    </header>;
-}
-export default Heder;
--- src/components/Musik/Musik.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import s from'./Musik.module.css';
-const Musik = ()=>{
-    return (
-      <div>Musik</div>
-
-    )
-}
-export default Musik;
